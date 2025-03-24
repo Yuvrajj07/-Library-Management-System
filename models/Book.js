b5830d22cc527bac3e5a0eec617539b6da47e85c
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-03-24 19:41:58 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
@@ -5,7 +6,26 @@ const bookSchema = new mongoose.Schema({
     bookAuthor: String,
     bookPages: Number,
     bookPrice: Number,
-    bookState: { type: String, default: "Available" }
+    bookState: { type: String, default: "Available" },
+    borrower: {
+        name: String,
+        phone: String,
+        email: String,
+        address: String
+    }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
+
+
+// const mongoose = require('mongoose');
+
+// const bookSchema = new mongoose.Schema({
+//     bookName: String,
+//     bookAuthor: String,
+//     bookPages: Number,
+//     bookPrice: Number,
+//     bookState: { type: String, default: "Available" }
+// });
+
+// module.exports = mongoose.model("Book", bookSchema);
